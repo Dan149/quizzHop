e243695b4579703d6a22386532b3a45bc3cab573
Romain Hérault <romain@ewill.fr>
2021-01-11 12:13:32 +0100

    test: testing users and pages

---
+++ tests/Controller/PagesControllerTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Tests\NeedLogin;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PagesControllerTest extends WebTestCase
+{
+    use FixturesTrait;
+    use NeedLogin;
+
+    public function setUp()
+    {
+        self::ensureKernelShutdown();
+    }
+
+    public function testHomepage()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextSame('h1', 'Create your own quizz and play with.');
+    }
+
+    public function testNavWithNoAccount()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextSame('#sign a:first-of-type', 'Sign in');
+        $this->assertSelectorTextSame('#sign a:last-of-type', 'Sign up');
+    }
+
+    /**
+     * @dataProvider userProvider
+     */
+    public function testNavWithConnectedAccount($user)
+    {
+        $client = static::createClient();
+
+        $this->login($client, $user);
+
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextSame('#sign a:first-of-type', 'My account');
+        $this->assertSelectorTextSame('#sign a:last-of-type', 'Logout');
+    }
+
+    public function userProvider()
+    {
+        $users = $this->loadFixtureFiles([
+            dirname(__DIR__) . '/fixtures/Users.yaml'
+        ]);
+
+        yield [$users['user_user']];
+        yield [$users['user_admin']];
+    }
+}
---
+++ tests/Repository/UserRepositoryTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Repository\UserRepository;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class UserRepositoryTest extends KernelTestCase
+{
+    use FixturesTrait;
+
+    public function testCountUsers()
+    {
+        self::bootKernel();
+
+        $users = $this->loadFixtureFiles([
+            dirname(__DIR__) . '/fixtures/Users.yaml'
+        ]);
+
+        $users = self::$container->get(UserRepository::class)->count([]);
+        $this->assertEquals(12, $users);
+    }
+}
