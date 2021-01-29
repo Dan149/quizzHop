ee72905136309860b59f66e640d9df0680539ba8
Romain <romain.herault99@gmail.com>
2021-01-29 16:34:49 +0100

    fix: purgecss remove generated class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   purge: [
-	  './templates/**/*.html.twig'
+    './templates/**/*.html.twig',
+	  
+    options: {
+      safelist: ['w-1/3'],
+    },
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
